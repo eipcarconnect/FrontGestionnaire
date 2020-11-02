1660cb56f2477906ab81f214c43380ef3047cf30
Augustin Leconte <augustin.leconte@epitech.eu>
2020-11-02 17:32:55 +0100

    add img, and start gluing oimage and form

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
 const express = require('express')
 const app = express();
+var path = require('path');
 const port = 8000;
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
-    res.sendFile('./src/html/index.html', {root: __dirname});
+    res.sendFile('./public/html/index.html', {root: __dirname});
 });
 
 app.get('/login', function(req, res){
-    res.sendFile('./src/html/Login.html', {root: __dirname});
+    res.sendFile('./public/html/Login.html', {root: __dirname});
 });
 
 app.get('/signin', function(req, res){
-    res.sendFile('./src/html/SignIn.html', {root: __dirname});
+    res.sendFile('./public/html/SignIn.html', {root: __dirname});
 });
 
 app.get('/factures', function(req, res){
-    res.sendFile('./src/html/Factures.html', {root: __dirname});
+    res.sendFile('./public/html/Factures.html', {root: __dirname});
 });
 
 app.get('/map', function(req, res){
-    res.sendFile('./src/html/Map.html', {root: __dirname});
+    res.sendFile('./public/html/Map.html', {root: __dirname});
 });
 
 app.get('/vehicules', function(req, res){
-    res.sendFile('./src/html/Vehicules.html', {root: __dirname});
+    res.sendFile('./public/html/Vehicules.html', {root: __dirname});
 });
 
 app.listen(port, () => {
