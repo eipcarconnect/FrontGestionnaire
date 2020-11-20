c97d98fc7dc60517c1c2be01d6eb94404918a04b
Augustin Leconte <augustin.leconte@epitech.eu>
2020-11-20 15:58:02 +0100

    fetch request WORKS

--- public/js/auth.js
+++ public/js/auth.js
@@ -3,48 +3,57 @@ api_url = "40.85.113.74:3000/"
 function signInUser()
 {
 	var form = document.getElementById("signinForm");
-	//var birthdate = form.getFullYear() + "-" + form.getMonth() + "-" + form.getDate();
 	var body = {
 		name: form.name,
 		email: form.email,
 		password: form.password,
-		birthdate: form.birthdate,
 		company: form.company
 	};
-	// var xmlHttp = new XMLHttpRequest();
-	// xmlHttp.open( "POST", api_url + "auth/manager/signup", true )
-	// xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	// xmlHttp.onload = function () {
-	// 	console.log(this);
-	// }
-	// xmlHttp.send(body);
-	fetch(api_url + "auth/manager/signup", {
-			method: "POST",
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(body)
-		}).then(r=>r.json()).then(r=> {
-			console.log(r)
-		}).then(error=>console.log(error))
+	// fetch(api_url + "auth/manager/signup", {
+	// 		method: "POST",
+	// 		headers: {
+	// 			'Content-Type': 'application/x-www-form-urlencoded'
+	// 		},
+	// 		body: body
+	// 	}).then(r=>r.json()).then(r=> {
+	// 		console.log(r)
+	// 	}).then(error=>console.log(error))
+		console.log(body);
+		fetch("http://40.85.113.74:3000/auth/manager/signup", {
+
+		    method: "POST",
+
+		    body: JSON.stringify({
+					name: "LECONTE Augustin",
+					email: "augustin.leconte@epitech.eu",
+					password: "azerty1234",
+					company: "testA1"
+		    }),
+
+		    headers: {
+		        "Content-type": "application/json; charset=UTF-8"
+		    }
+		})
+		.then(response => response.json())
+		.then(json => console.log(json));
+
 }
 
-function logInUser()
-{
-	var form = document.getElementById("loginForm");
-	var body = {
-		email: form.email,
-		password: form.password,
-	};
-	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.onload = function () {
-		console.log(this);
-	}
-	xmlHttp.open( "POST", api_url + "auth/manager/signin", true )
-	xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	xmlHttp.send(body);
-}
+// function logInUser()
+// {
+// 	var form = document.getElementById("loginForm");
+// 	var body = {
+// 		email: form.email,
+// 		password: form.password,
+// 	};
+// 	var xmlHttp = new XMLHttpRequest();
+// 	xmlHttp.onload = function () {
+// 		console.log(this);
+// 	}
+// 	xmlHttp.open( "POST", api_url + "auth/manager/signin", true )
+// 	xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+// 	xmlHttp.send(body);
+// }
 
 function moveImg(val) {
 		var page = "https://front-gestionnaire-web.herokuapp.com";
