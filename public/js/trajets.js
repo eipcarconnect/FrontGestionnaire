611856bfa6dccdf06a0b17d1396f947439abf7d9
Seub <you@example.com>
2020-12-02 23:51:21 +0100

    adding research to each tabs

--- public/js/trajets.js
+++ public/js/trajets.js
@@ -16,18 +16,77 @@ function getCompanyRides()
 			let content = document.getElementById("trajets_content");
 			let tmp = "";
 			let f = 0;
+
+			let key = document.getElementById('search-trajet').value
+			let selection = document.getElementById('sort-selection-trajet').value
+
+			switch (selection) {
+				case "Nom par ordre croissant":
+					json["rides"].sort(function(a,b) {
+						return b["name"] < a["name"]
+					});
+					break;
+				case "Nom par ordre decroissant":
+					json["rides"].sort(function(a,b) {
+						return b["name"] > a["name"]
+					});
+					break;
+				case "Véhicule par ordre croissant":
+					json["rides"].sort(function(a,b) {
+						return b["vehicleLicencePlate"] < a["vehicleLicencePlate"]
+					});
+					break;
+				case "Véhicule par ordre decroissant":
+					json["rides"].sort(function(a,b) {
+						return b["vehicleLicencePlate"] > a["vehicleLicencePlate"]
+					});
+					break;
+				case "Employé par ordre croissant":
+					json["rides"].sort(function(a,b) {
+						return b["userId"] < a["userId"]
+					});
+					break;
+				case "Employé par ordre decroissant":
+					json["rides"].sort(function(a,b) {
+						return b["userId"] > a["userId"]
+					});
+					break;
+				case "Date par ordre croissant":
+					json["rides"].sort(function(a,b) {
+						let parts_a = a["date"].split("/");
+						let tmp_a = new Date(parts_a[2], parts_a[1] - 1, parts_a[0]);
+						let parts_b = b["date"].split("/");
+						let tmp_b = new Date(parts_b[2], parts_b[1] - 1, parts_b[0]);
+						return tmp_b < tmp_a
+					});
+					break;
+				case "Date par ordre decroissant":
+					json["rides"].sort(function(a,b) {
+						let parts_a = a["date"].split("/");
+						let tmp_a = new Date(parts_a[2], parts_a[1] - 1, parts_a[0]);
+						let parts_b = b["date"].split("/");
+						let tmp_b = new Date(parts_b[2], parts_b[1] - 1, parts_b[0]);
+						return tmp_b > tmp_a
+					});
+					break;
+				default:
+					break;
+			}
+
 			json["rides"].forEach((e,i)=>{
+				if (e["name"].includes(key) || e["vehicleLicencePlate"].includes(key) || e["start"].includes(key) || e["end"].includes(key) || e["date"].includes(key) ) {
 					tmp +=  "<tr scope='row'>" +
-					"<td scope='col'>"+e["name"]+"</td>" +
-					"<td scope='col'>"+e["vehicleLicencePlate"]+"</td>" +
-					"<td scope='col' id="+e["userId"]+ f + "></td>" +
-					"<td scope='col'>"+e["start"]+"</td>" +
-					"<td scope='col'>"+e["end"]+"</td>" +
-					"<td scope='col'>"+e["date"]+"</td>" +
-					"<td scope='col'><a href='factures/"+e["_id"]+"'/>Facture</a></td></tr>";
+						"<td scope='col'>"+e["name"]+"</td>" +
+						"<td scope='col'>"+e["vehicleLicencePlate"]+"</td>" +
+						"<td scope='col' id="+e["userId"]+ f + "></td>" +
+						"<td scope='col'>"+e["start"]+"</td>" +
+						"<td scope='col'>"+e["end"]+"</td>" +
+						"<td scope='col'>"+e["date"]+"</td>" +
+						"<td scope='col'><a href='factures/"+e["_id"]+"'/>Facture</a></td></tr>";
 					content.innerHTML = tmp;
 					getUNIQUEUser(e["userId"], f);
 					f+=1;
+				}
 			})
 		});
 }
--- public/js/users.js
+++ public/js/users.js
@@ -15,11 +15,42 @@ function getUsers()
 			document.getElementById("nbUsers").innerHTML = "Employés : " + json["users"].length;
 			let content = document.getElementById("users_content");
 			let tmp = "";
+
+			let key = document.getElementById('search-user').value
+			let selection = document.getElementById('sort-selection-user').value
+
+			switch (selection) {
+				case "Nom par ordre croissant":
+					json["users"].sort(function(a,b) {
+						return b["name"] < a["name"]
+					});
+					break;
+				case "Nom par ordre decroissant":
+					json["users"].sort(function(a,b) {
+						return b["name"] > a["name"]
+					});
+					break;
+				case "Email par ordre croissant":
+					json["users"].sort(function(a,b) {
+						return b["email"] < a["email"]
+					});
+					break;
+				case "Email par ordre decroissant":
+					json["users"].sort(function(a,b) {
+						return b["email"] > a["email"]
+					});
+					break;
+				default:
+					break;
+			}
+
 			json["users"].forEach((e,i)=>{
-					tmp +=  "<tr scope='row'>" +
-					"<td scope='col'>"+e["name"]+"</td>" +
-					"<td scope='col'>"+e["email"]+"</td></tr>";
+				if (e["name"].includes(key) || e["email"].includes(key) ) {
+					tmp += "<tr scope='row'>" +
+						"<td scope='col'>" + e["name"] + "</td>" +
+						"<td scope='col'>" + e["email"] + "</td></tr>";
 					content.innerHTML = tmp;
+				}
 			})
 		});
 }
