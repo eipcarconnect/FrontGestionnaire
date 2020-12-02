546a1363996bfbef941427a878f9cc6dea30059b
Augustin Leconte <augustin.leconte@epitech.eu>
2020-12-02 21:47:01 +0100

    add userin ride list

--- public/js/trajets.js
+++ public/js/trajets.js
@@ -12,22 +12,47 @@ function getCompanyRides()
 		})
 		.then(response => response.json())
 		.then(json => {
-			console.log(json)
+			document.getElementById("nbRides").innerHTML = "Trajets : " + json["rides"].length;
 			let content = document.getElementById("trajets_content");
 			let tmp = "";
+			let f = 0;
 			json["rides"].forEach((e,i)=>{
-					console.log(e);
 					tmp +=  "<tr scope='row'>" +
 					"<td scope='col'>"+e["name"]+"</td>" +
 					"<td scope='col'>"+e["vehicleLicencePlate"]+"</td>" +
-					"<td scope='col'>"+e["userId"]+"</td>" +
+					"<td scope='col' id="+e["userId"]+ f + "></td>" +
 					"<td scope='col'>"+e["start"]+"</td>" +
 					"<td scope='col'>"+e["end"]+"</td>" +
 					"<td scope='col'>"+e["date"]+"</td>" +
 					"<td scope='col'><a href='factures/"+e["_id"]+"'/>Facture</a></td></tr>";
 					content.innerHTML = tmp;
+					getUNIQUEUser(e["userId"], f);
+					f+=1;
 			})
 		});
+}
+
+function getUNIQUEUser(userID, f)
+{
+	var token = localStorage.getItem('token');
+	fetch("http://40.85.113.74:3000/data/manager/getusers", {
+			method: "POST",
+			body: JSON.stringify({
+				token: token,
+			}),
+			headers: {
+					"Content-type": "application/json; charset=UTF-8"
+			}
+	})
+	.then(response => response.json())
+	.then(json => {
+		let content = document.getElementById("users_content");
+		let tmp = "";
+		json["users"].forEach((e,i)=>{
+			if (userID == e["_id"])
+				document.getElementById(userID + "" + f).innerHTML = e["name"];
+		})
+	});
 }
 
 getCompanyRides();
