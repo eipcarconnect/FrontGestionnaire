60b96720fb15f6e7a1a4f16ec065cc20ffa82ab5
Augustin Leconte <augustin.leconte@epitech.eu>
2020-12-03 00:23:46 +0100

    try to fix https error

--- public/js/auth.js
+++ public/js/auth.js
@@ -2,7 +2,7 @@ function signInUser()
 {
 	var token;
 	var form = document.getElementById("signinForm");
-	fetch("http://40.85.113.74:3000/auth/manager/signup", {
+	fetch("https://40.85.113.74:3000/auth/manager/signup", {
 	    method: "POST",
 	    body: JSON.stringify({
 				name: form.name.value,
@@ -22,7 +22,7 @@ function logInUser()
 {
 	var token;
 	var form = document.getElementById("loginForm");
-	fetch("http://40.85.113.74:3000/auth/manager/signin", {
+	fetch("https://40.85.113.74:3000/auth/manager/signin", {
 			method: "POST",
 			body: JSON.stringify({
 				email: form.email.value,
